70e3586d184ba356150f7f9c10aa70fb4754cd18
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-25 13:24:27 +0200

    creation variable livres si existant

--- server.js
+++ server.js
@@ -11,9 +11,30 @@ app.use(bodyparser.urlencoded({
 }));
 //Choix du moteur de template
 app.set('view engine','ejs');
+//Ouverture de la base de données
+var db= new sqlite3.Database('./mediatheque',sqlite3.OPEN_READWRITE, (err)=> {
+	if (err){
+		console.log(err.message);
+	}
+	console.log('Access database success')
+});
 //Page d'accueil du site 
 app.get('/',function(req,res){
-	res.render('index');
+	var affich= "SELECT Nom,auteur,Annee,Genre FROM Livre ORDER BY Annee DESC LIMIT 10";
+	db.serialize(()=>{
+		db.all(affich,(err,row)=>{
+			if (err){
+				console.log(err.message)
+			};
+			if(row.length>0){
+				console.log(row.length+' livres ')
+				res.render('index',{llivres: row})
+			}else {
+				console.log('Pas de livres')
+				res.render('index')
+			};
+		})
+	})
 });
 //Page de connexion du site
 app.get('/signin',function(req,res){
@@ -25,6 +46,7 @@ app.get('/signup',function(req,res){
 });
 //Au moment de l'envoi d'un formulaire d'inscription
 app.post('/signup',function(req,res){
+
 	res.render('index');
 });
 
